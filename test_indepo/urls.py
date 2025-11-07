b388d9b42ac3edc19297ba8a231932312e539718
p0pc0rn0 <ikhedit@gmail.com>
2025-11-07 09:38:13 +0300

    Локальные изменения urls.py

--- test_indepo/urls.py
+++ test_indepo/urls.py
@@ -15,20 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    # filer лучше вынести сюда, вне i18n, чтобы не было /ru/filer/
+    path('filer/', include('filer.urls')),
+]
+
+urlpatterns += i18n_patterns(
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('admin/', admin.site.urls),
-    path('filer/', include('filer.urls')),
     path('', include('cms.urls')),
 )
 
-
+# медиафайлы вне i18n — правильно
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
